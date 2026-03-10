bd3956271ea039b8a5033930986181c79d1e3ef3
Ajeet Chouksey <ajeetchouksey4@gmail.com>
2026-03-10 18:35:50 +0530

    feat: add EnquiryRepository for database operations

---
+++ src/main/java/in/rajeevgandhischool/backend/repository/EnquiryRepository.java
@@ -0,0 +1,9 @@
+package in.rajeevgandhischool.backend.repository;
+
+import in.rajeevgandhischool.backend.entity.Enquiry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnquiryRepository extends JpaRepository<Enquiry, Long> {
+}
